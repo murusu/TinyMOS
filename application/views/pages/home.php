7a437160a0779c3fa60dcdf6017fad90eeb279ac
murusu <murusu0.0@gmail.com>
2012-05-18 17:21:00 +0800

    start up
    Signed-off-by: murusu <murusu0.0@gmail.com>

---
+++ application/views/pages/home.php
@@ -0,0 +1,9 @@
+<body>
+<div class="login_main center">
+	<div class="title"><h1>this is a test</h1></div>
+	<div class="input_bar center"><div class="input_left">User Name:</div><div class="input_right"><input class="input_form" type="input" name="title"></div><div class="clear"></div></div>
+	<div class="input_bar center"><div class="input_left">Password:</div><div class="input_right"><input class="input_form" type="password" name="title"></div><div class="clear"></div></div>
+	<div class="input_bar center title">this is a test messae</div>
+	<div class="input_bar center"><div class="center" style="width:20%" ><input type="submit" value="Login" name="submit"></div></div>
+</div>	
+</body>
---
+++ application/views/templates/header.php
@@ -0,0 +1,17 @@
+<?php $this->load->helper('url'); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>static/css/base.css" />
+<!--------------
+<link rel="stylesheet" type="text/css" media="screen and (max-device-width: 400px)" href="<?php echo base_url(); ?>static/css/tinyScreen.css" />
+<link rel="stylesheet" type="text/css" media="screen and (min-width: 400px) and (max-device-width: 600px)" href="<?php echo base_url(); ?>static/css/smallScreen.css" />
+---------------->
+<!--[if lt IE 9]>
+　<script src="<?php echo base_url(); ?>static/js/css3-mediaqueries.js"></script>
+<![endif]-->
+<title>test</title>
+</head>
+	
